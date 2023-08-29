d8799a57c99f5877ddc755efff3583e657913ca0
hshima101 <67283100+hshima101@users.noreply.github.com>
2023-08-29 01:32:05 -0400

    Add files via upload
    A more cleaned up version of the code. Test code has been removed to make the code easier to understand. Code is designed to be a simple class based object oriented program made as a joke for Cohort 6. For anyone who is viewing this from Cohort 6, I hope this code can help you better understand how to structure and organize your code in future assignments.

--- questions.cpp
+++ questions.cpp
@@ -9,10 +9,6 @@
 //default constructor
 Mike::Mike()
 {
-    /*Working Functions*/
-    //getdata();
-    //setdata();
-    //check();
     //load number from file
     //save number in variable
     //modify the variable until function quits
@@ -25,10 +21,6 @@ Mike::Mike()
     tempQuestions = tempQuestions + number;
     writeToFile("test.txt", tempQuestions);
     std::cout<<tempQuestions<<std::endl;
-    //modify temp questions 
-    
-    
-    /*Test Functions*/
 }
 
 //destructor
@@ -37,53 +29,7 @@ Mike::~Mike(void)
 
 };
 
-
-void Mike::getdata()
-{
-    std::cout<<"Mike has told you to ask him questions "<<baseQuestions<<" times"<<std::endl;
-    std::cout<<"press 1 if he has said it again"<<std::endl;
-    std::cout<<"press 2 to quit program"<<std::endl;
-    std::cin>>button;
-    std::cout<<std::endl;
-
-    readFile("test.txt");
-    writeFile("test.txt");
-    readFile("test.txt");
-}
-
-void Mike::setdata() const
-{
-    std::cout<<"Mike has told you to ask him questions "<<questions<<" times"<<std::endl;
-}
-
-bool Mike::binary(int input)
-{
-    if(input == 1)
-    {
-        std::cout<<"True"<<std::endl;
-        return true;
-
-    } else if(input == 2) {
-
-        std::cout<<"False"<<std::endl;
-        return false;
-    }
-    return 0;
-}
-
-bool Mike::check()
-{
-    if(this->button == 1)
-    {
-        std::cout<<"true"<<std::endl;
-        this -> questions = this -> questions + 1;
-        return true;
-    } else {
-        std::cout<<"false"<<std::endl;
-        return false;
-    }
-}
-
+//function used to increment numbers by keypress
 bool Mike::breakOnKeyPress(char keyToBreak)
 {
     initscr(); // initialize ncurses
@@ -110,6 +56,7 @@ bool Mike::breakOnKeyPress(char keyToBreak)
     return false;
 }
 
+//function opens .txt file and writes to file
 void Mike::writeToFile(const std::string& filename, int input)
 {
     std::cout<<"Writing data to file"<<std::endl;
@@ -128,75 +75,6 @@ void Mike::writeToFile(const std::string& filename, int input)
 
     //close the file
     outFile.close();
-}
-
-//Read and write file functions
-void Mike::writeFile(const std::string& filename)
-{
-    std::cout<<"Enter content you want to write to the file (type 'EOF' on a new line to finish): "<<std::endl;
-
-    //read user input line by line until they type 'EOF'
-    while(true)
-    {
-        std::string line;
-        std::getline(std::cin, line);
-        if(line == "EOF")
-        {
-            break;
-        }
-
-        content += line + "\n";
-    }
-
-    //create an ofstream object to write the file to
-    std::ofstream outFile(filename);
-
-    //check if it was successfully created
-    if(!outFile.is_open())
-    {
-        std::cerr<<"Error opening the file for writing: "<< filename <<std::endl;
-        return;
-    }
-
-    //write the content to the file
-    outFile << content;
-
-    std::cout<<"Content has been written to the file"<<std::endl;
-
-    //close the file
-    outFile.close();
-
-}
-
-
-std::string Mike::readFile(const std::string& filename)
-{
-    std::cout<<"readFile invoked"<<std::endl;
-    std::ifstream inFile(filename);
-
-    //check if the file was successfully opened
-    if(!inFile.is_open())
-    {
-        std::cerr << "Error opening file for reading: " << filename << std::endl;
-        return "";
-    }
-
-    std::string content;
-    std::string line;
-
-    //read the file by line and append it to the content string
-    while (std::getline(inFile, line))
-    {
-        content += line + "\n";
-    }
-    
-    //close the file
-    inFile.close();
-
-    std::cout<<"Content Read from file"<<std::endl;
-    std::cout<<content<<std::endl;
-
-    return content;
 }
 
 //read and save to program
--- questions.h
+++ questions.h
@@ -13,27 +13,14 @@ class Mike
     public:
         Mike();
         ~Mike(void);
-        void getdata();
-        void setdata() const;
-        bool binary(int input);
 
-        //test functions
-        bool check();
-
-        //test logic loops
         bool breakOnKeyPress(char keyToBreak);
-
-        //bool check(int input);
-
-        void writeFile(const std::string& filename);
 
         void writeToFile(const std::string& filename, int input);
 
-        std::string readFile(const std::string& filename);
-
         bool readNumberFromFile(const std::string& filename, int& number);
 
-    private:
+   private:
         std::string content;
         int number = 0;
         int questions = 0;
